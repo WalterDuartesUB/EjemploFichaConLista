7bb5891d49abbf853d2370663fa507aa2c11cc2c
wduartes <walterduartes@gmail.com>
2018-06-06 15:44:41 -0300

    Se refactoriza la clase ABMAerolineasView

--- src/ar/edu/ub/p3/controlador/Aplicacion.java
+++ src/ar/edu/ub/p3/controlador/Aplicacion.java
@@ -4,7 +4,7 @@ import ar.edu.ub.p3.vista.ABMAerolineasView;
 public class Aplicacion {
 
 	public static void main(String[] args) {
-		new ABMAerolineasView();	
+		new ABMAerolineasView( new ABMAerolineasControlador() );	
 	}
 
 }
--- src/ar/edu/ub/p3/vista/ABMAerolineasView.java
+++ src/ar/edu/ub/p3/vista/ABMAerolineasView.java
@@ -8,35 +8,63 @@ import ar.edu.ub.p3.controlador.ABMAerolineasControlador;
 import ar.edu.ub.p3.vista.paneles.ABMAerolineaPanel;
 import ar.edu.ub.p3.vista.paneles.ListaAerolineasPanel;
 
-public class ABMAerolineasView {
-	public ABMAerolineasView() {
-		JFrame frame = new JFrame();
-		
-		frame.setLayout( new GridLayout(1,2) );		
-
-		ABMAerolineasControlador controladorFicha = new ABMAerolineasControlador();		
-		ListaAerolineasPanel 	 panelLista = new ListaAerolineasPanel();			
-		ABMAerolineaPanel        panelFicha = new ABMAerolineaPanel();
-						
-		panelLista.setInput( controladorFicha.getAerolineas() );
-		
-		panelFicha.addListener( controladorFicha );
-		controladorFicha.addListener( panelLista );
-		
-		this.addLayout(frame, panelLista, panelFicha);
-		
-		//Fuerzo unos datos iniciales para la ficha
-		panelFicha.getViewModel().setIdAerolinea("AAAA");
-		panelFicha.getViewModel().setNombre("Aeropuertos 2018");
+public class ABMAerolineasView extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private ListaAerolineasPanel 	 panelLista;			
+	private ABMAerolineaPanel        panelFicha;
+	private ABMAerolineasControlador controladorFicha;
+	
+	public ABMAerolineasView( ABMAerolineasControlador controladorFicha ) {
+		this.setControladorFicha(controladorFicha);
+		this.addComponent();		
+		this.addLayout();
 	}
 
-	private void addLayout(JFrame frame, ListaAerolineasPanel panelLista, ABMAerolineaPanel panelFicha) {
-		frame.add( panelLista );
-		frame.add( panelFicha );
-				
-		frame.setSize(700, 600);
-		frame.setVisible(true);
+	private void addComponent() {
+		this.setPanelFicha( new ABMAerolineaPanel() );
+		this.setPanelLista( new ListaAerolineasPanel() );	
 		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.getPanelLista().setInput( this.getControladorFicha().getAerolineas() );
+		
+		this.getPanelFicha().addListener( this.getControladorFicha() );
+		this.getControladorFicha().addListener( this.getPanelLista() );		
+	}
+
+	private void addLayout() {
+		this.setLayout( new GridLayout(1,2) );		
+		this.add( this.getPanelLista() );
+		this.add( this.getPanelFicha() );
+				
+		this.setSize(700, 600);
+		this.setVisible(true);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public ABMAerolineaPanel getPanelFicha() {
+		return panelFicha;
+	}
+
+	public void setPanelFicha(ABMAerolineaPanel panelFicha) {
+		this.panelFicha = panelFicha;
+	}
+
+	public ListaAerolineasPanel getPanelLista() {
+		return panelLista;
+	}
+
+	public void setPanelLista(ListaAerolineasPanel panelLista) {
+		this.panelLista = panelLista;
+	}
+
+	public ABMAerolineasControlador getControladorFicha() {
+		return controladorFicha;
+	}
+
+	public void setControladorFicha(ABMAerolineasControlador controladorFicha) {
+		this.controladorFicha = controladorFicha;
 	}
 }
